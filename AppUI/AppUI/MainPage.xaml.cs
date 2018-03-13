1be2dda42cd22a50d532053a3bd188070f4a7bae
Hana Ben Hadj Abdallah <h.benhadj@root.sa>
2018-03-13 14:32:18 +0100

    Add project files.

---
+++ AppUI/AppUI/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using AppUI.Model;
+using AppUI.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppUI
+{
+	public partial class MainPage : MasterDetailPage
+	{
+        public MainPage()
+        {
+            InitializeComponent();
+            Detail = new Page1();
+            
+            List<List> L = new List<List>
+            {
+               new List { Title = "My ACCOUNT" },
+               new List {Title ="SHOP"},
+               new List {Title ="FAQ"},
+               new List {Title ="NOTIFICATION" , Notification="2"}
+            };
+            ListView1.ItemsSource = L;
+
+
+        }
+
+
+
+        void SeelectedItem(object sender, EventArgs e)
+        {
+            var l = ListView1.SelectedItem as List;
+
+            if ( l.Title == "My ACCOUNT")
+            {
+                Detail = new MyAccount();
+                IsPresented = false;
+               
+            }
+            if (l.Title == "Shop")
+            {
+                Detail = new Shop();
+                IsPresented = false;
+            }
+            if (l.Title == "FAQ")
+            {
+                Detail = new FAQ();
+                IsPresented = false;
+            }
+            if (l.Title == "Notification")
+            {
+                Detail = new Notification();
+                IsPresented = false;
+            }
+
+
+        }
+    }
+}
---
+++ AppUI/AppUI/Model/List.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace AppUI.Model
+{
+   public  class List
+    {
+       public String Title { get; set; }
+        public String Notification { get; set; }
+
+        //private string v;
+
+        //public List(string v)
+        //{
+        //    this.Title = v;
+        //}
+        //private int s;
+
+        //public List(string v, int s)
+        //{
+        //    this.Title = v;
+        //    this.Notification = s;
+        //}
+
+
+    }
+  
+}
